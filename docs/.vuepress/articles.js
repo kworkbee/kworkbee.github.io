d6ef705673429def651c91c48d916c1bed9e3580
G1 <g1.tommy.jeon@gmail.com>
2022-06-22 21:18:51 +0900

    docs: New Post

--- docs/.vuepress/articles.js
+++ docs/.vuepress/articles.js
@@ -7,6 +7,7 @@ exports.sidebar = [
                   { text: 'Security Architecture / Code Analysis', link: '/backend/spring/2022/05/31/spring-security-architecture-code-analysis' },
                   { text: 'R2DBC ConnectionFactory', link: '/backend/spring/2022/06/09/r2dbc-connectionfactory' },
                   { text: 'Gradle Docker Compose Plugin', link: '/backend/spring/2022/06/10/gradle-docker-compose-plugin' },
+                  { text: 'Spring Custom Annotation', link: '/backend/spring/2022/06/22/spring-custom-annotation' },
               ] },
         ]
     },
